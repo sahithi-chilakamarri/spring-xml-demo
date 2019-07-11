42db1ca6e69c4f618afa2018285ec9d56e11425d
Sahithi <sahithimpc@gmail.com>
2019-07-11 22:26:41 +0530

    This is y commit for task2

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,43 +1,27 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class Main {
     public static void main(String[] args) {
-        //Dependency injection using Application Context
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        System.out.println("Config file loaded");
-        Movie movie1= context.getBean("geethagovindham", Movie.class);
-        System.out.println("Created dependencies using application context");
-        movie1.display();
-        System.out.println(".........");
-        
-        //Dependency Injection using Bean Factory
-        Resource resource = new ClassPathResource("beans.xml");
-        BeanFactory beanFactory = new XmlBeanFactory(resource);
-        Movie movie2 = context.getBean("kuchkuchhotahain", Movie.class);
-        System.out.println("Created dependencies using XmlBeanFactory");
+        //Injecting movie2 object
+        Movie movie2=context.getBean("movie2",Movie.class);
         movie2.display();
-        System.out.println(".........");
-
-        //Dependency Injection using BeanDefinitionRegistry and BeanDefinitionReader.
-        BeanDefinitionRegistry beanDefinitionRegistry = new GenericApplicationContext((DefaultListableBeanFactory) beanFactory);
-        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions("beans.xml");
-        Movie movie3 = beanFactory.getBean("HaseeTohPhasee", Movie.class);
-        System.out.println("Created dependencies using BeanDefinitionRegistry and BeanDefinitionReader");
+        //Injecting movie3 object
+        Movie movie3=context.getBean("movie3",Movie.class);
         movie3.display();
-        System.out.println("..........");
+        //Checking singleton scope
+        System.out.println(movie2==movie3);
+        //Injecting movie4 object
+        Movie movie4=context.getBean("movie4",Movie.class);
+        movie4.display();
+        //Injecting movie5 object
+        Movie movie5=context.getBean("movie5",Movie.class);
+        movie5.display();
+        //Checking prototype scope
+        System.out.println(movie4==movie5);
     }
 }
